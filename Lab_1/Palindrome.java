bde089c38dba599d42d92551411a34593b37999d
NikitaKiryaev <64013226+NikitaKiryaev@users.noreply.github.com>
2020-04-22 03:51:03 +0300

    Lab_1 KTP Kiryaev BVT1802
    Выполненная первая лабораторная работа

---
+++ Lab_1/Palindrome.java
@@ -0,0 +1,43 @@
+// Класс Palindrome
+public class Palindrome {
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			String s = args[i];
+			if (isPalindrome(args[i])) {                //Выводи сообщение если слово палиндроми или нет.
+				System.out.println("Palindrome " + s);
+			
+				
+				}
+			else
+				System.out.println("Ne Palindrome " + s);
+			}
+			
+		}
+		
+	
+//Функция переворачивания строки
+	public static String reverseString(String s){
+		String r = "";
+		for (int l = s.length() - 1; l >= 0; --l) {
+			r += s.charAt(l);
+		}
+		return r;
+	}
+
+//Функция проверки строки на палиндром
+	public static Boolean isPalindrome (String s){    
+		if (s.equals(reverseString(s))) {
+			return true;
+			
+		}
+		return false;
+
+	}
+
+
+
+
+
+
+
+}
